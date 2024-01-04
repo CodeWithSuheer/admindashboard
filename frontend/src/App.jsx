d77188685a36ed94f73f16dbe6919809a5bb6cfe
Arqum01 <arqumahmad019@gmail.com>
2024-01-04 11:07:35 +0500

    New 4 Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Home/HomePage";
 import "./App.css";
+import Login from "./Pages/Login/Login";
+import SignUp from "./Pages/SignUp/SignUp";
+import ForgetPassword from "./Pages/Login/ForgetPassword";
+import ResetPassword from "./Pages/ResetPassword/ResetPassword";
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgetpass" element={<ForgetPassword />} />
+          <Route path="/resetpass" element={<ResetPassword />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </>
